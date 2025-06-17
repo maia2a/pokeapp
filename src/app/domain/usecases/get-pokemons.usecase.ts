171dd63ade953427c0b2f04062f3a178853e301b
Gabriell Maia do Amaral Duarte <gabrielldeveloper@gmail.com>
2025-06-17 01:40:26 -0300

    feat(usecase): add get pokemons use case for fetching pokemon list

---
+++ src/app/domain/usecases/get-pokemons.usecase.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Pokemon } from '../models/pokemon.model';
+import { PokemonRepository } from '../models/pokemon.repository';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GetPokemonUseCase {
+  constructor(private pokemonRepository: PokemonRepository) {}
+
+  /**
+   * Executa o caso de uso para obter a lista de Pokémons.
+   * @param offset - O ponto de início da busca.
+   * @param limit - O número de itens a serem buscados.
+   */
+  execute(offset: number, limit: number): Observable<Pokemon[]> {
+    return this.pokemonRepository.getPokemons(offset, limit);
+  }
+}
