4a39cda321a6466081dea84d8ae2d8da781cc4ac
Gabriell Maia <gabrielldeveloper@gmail.com>
2025-06-18 14:22:58 -0300

    feat(pokemon.repository): enhance getPokemons method to include pagination support
    Update the getPokemons method in the PokemonRepository to support pagination, allowing for more efficient data retrieval and improved user experience when displaying Pokémon lists.

---
+++ src/app/presentation/pages/pokemon-detail/pokemon-detail.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { PokemonDetail } from 'src/app/domain/models/pokemon-detail.model';
+import { GetPokemonByIdUseCase } from 'src/app/domain/usecases/get-pokemon-by-id.usecase';
+
+@Component({
+  selector: 'app-pokemon-detail',
+  templateUrl: './pokemon-detail.page.html',
+  styleUrl: './pokemon-detail.page.scss',
+})
+export class PokemonDetailPage implements OnInit {
+  pokemon$!: Observable<PokemonDetail>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private getPokemonByIdUseCase: GetPokemonByIdUseCase
+  ) {}
+
+  ngOnInit() {
+    const pokemonId = this.route.snapshot.paramMap.get('id');
+    if (pokemonId) {
+      this.pokemon$ = this.getPokemonByIdUseCase.execute(+pokemonId);
+    }
+  }
+}
