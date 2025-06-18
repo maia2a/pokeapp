6dfe71a7e172f5ed0d5c546175b61cf7b4abd720
Gabriell Maia <gabrielldeveloper@gmail.com>
2025-06-18 15:34:15 -0300

    feat(pokemon-detail): create PokemonDetailPage module with routing configuration
    Add a new module for the PokemonDetailPage, including necessary imports and routing setup to facilitate navigation to the Pokémon detail view.

---
+++ src/app/presentation/pages/pokemon-detail/pokemon-detail.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { PokemonDetailPage } from './pokemon-detail.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([{ path: '', component: PokemonDetailPage }]),
+    PokemonDetailPage,
+  ],
+})
+export class PokemonDetailPageModule {}
