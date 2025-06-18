42e325051bbdd1046d6a863fd783cdf762e9dd6e
Gabriell Maia <gabrielldeveloper@gmail.com>
2025-06-18 18:37:45 -0300

    feat(pokemon-detail): add favorite toggle functionality
    Implement favorite toggle feature in pokemon detail page using FavoritesService

--- src/app/presentation/pages/pokemon-detail/pokemon-detail.page.ts
+++ src/app/presentation/pages/pokemon-detail/pokemon-detail.page.ts
@@ -2,8 +2,10 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
+import { FavoritesService } from 'src/app/core/services/favorites.service';
 import { PokemonDetail } from 'src/app/domain/models/pokemon-detail.model';
+import { Pokemon } from 'src/app/domain/models/pokemon.model';
 import { GetPokemonByIdUseCase } from 'src/app/domain/usecases/get-pokemon-by-id.usecase';
 
 @Component({
@@ -15,16 +17,37 @@ import { GetPokemonByIdUseCase } from 'src/app/domain/usecases/get-pokemon-by-id
 })
 export class PokemonDetailPage implements OnInit {
   pokemon$!: Observable<PokemonDetail>;
+  isFavorite$!: Observable<boolean>;
 
   constructor(
     private route: ActivatedRoute,
-    private getPokemonByIdUseCase: GetPokemonByIdUseCase
+    private getPokemonByIdUseCase: GetPokemonByIdUseCase,
+    private favoritesService: FavoritesService
   ) {}
 
   ngOnInit() {
     const pokemonId = this.route.snapshot.paramMap.get('id');
     if (pokemonId) {
       this.pokemon$ = this.getPokemonByIdUseCase.execute(+pokemonId);
+
+      this.isFavorite$ = this.favoritesService.isFavorite(+pokemonId);
     }
+  }
+  toggleFavorite() {
+    const pokemonId = +this.route.snapshot.paramMap.get('id')!;
+    this.pokemon$.pipe(take(1)).subscribe((pokemon) => {
+      this.isFavorite$.pipe(take(1)).subscribe((isFav) => {
+        if (isFav) {
+          this.favoritesService.removeFavorite(pokemonId);
+        } else {
+          const basicPokemon: Pokemon = {
+            id: pokemon.id,
+            name: pokemon.name,
+            imageUrl: pokemon.sprites.front_default,
+          };
+          this.favoritesService.addFavorite(basicPokemon);
+        }
+      });
+    });
   }
 }
